de719ae7d002ba06eee2aeb0ab274856ece03b05
YuliaGrinenko <yuliaazarova1@gmail.com>
2024-02-17 13:06:33 +0000

    Api intergated, current temperature added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchApi(city) {
+  let apiKey = "df336e1036o064a2a04903f20318tb6d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchCity(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input").value;
-  let city = document.querySelector("#current-city");
-  city.innerHTML = searchInputElement;
+  searchApi(searchInputElement);
 }
 
 let searchFormElement = document.querySelector("#search-form");
